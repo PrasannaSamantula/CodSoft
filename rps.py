6d99a7624dc707fb569797c82b908704e995ca18
Samantula Gowri Prasanna <149043512+PrasannaSamantula@users.noreply.github.com>
2024-08-02 14:51:43 +0530

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,37 @@
+import random
+
+while True:
+    choices = ["rock", "paper", "scissors"]
+
+    computer = random.choice(choices)
+    player = False
+
+    while player not in choices:
+        player=input("rock,paper or scissors:").lower()
+
+    print("Computer\'s choice:",computer)
+    print("Your choice:",player)
+
+    if player == computer:
+        print("Tie!")
+    elif player == "rock":
+        if computer == "paper":
+            print("You lose!")
+        if computer == "scissors":
+            print("You won!!")
+    elif player == "scissors":
+        if computer == "rock":
+            print("You lose!")
+        if computer== "paper":
+            print("You won!!")
+    elif player == "paper":
+        if computer == "scissors":
+            print("You lose!")
+        if computer == "rock":
+            print("You won!!")
+
+    play_again=input("Play again(yes/no):").lower()
+    if play_again != "yes":
+        break
+
+print("BYE!!!")
